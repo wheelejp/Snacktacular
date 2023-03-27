bb8183d99137b21c2670d7c1a805dbb95390a5ed
Jonathan Wheeler <78514430+wheelejp@users.noreply.github.com>
2023-03-27 11:03:32 -0400

    Initial Commit

---
+++ Snacktacular/SnacktacularApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularApp.swift
+//  Snacktacular
+//
+//  Created by Jonathan Wheeler Jr. on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
